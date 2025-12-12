dbbccf75cbcb44f5ec41eb1d82530abf4fa1109a
WaizImran <waizimran2009@gmail.com>
2025-12-12 19:49:42 +0500

    Refactor ingredient selection and search logic

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,20 +30,20 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 #st.dataframe(pd_df)
 #st.stop()
 ingredients_list = st.multiselect(
-'Choose up to 5 ingredients:'
-,my_dataframe
-,max_selections=5
+ 'Choose up to 5 ingredients:'
+ ,my_dataframe
+ ,max_selections=5
  )
 if ingredients_list:
-ingredients_string = ''
-for fruit_chosen in ingredients_list:
-ingredients_string += fruit_chosen +' '
+ ingredients_string = ''
+  for fruit_chosen in ingredients_list:
+    ingredients_string += fruit_chosen +' '
 
-search_on=pd_df.loc [pd_df ['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-
-st.write('The search value for', fruit_chosen,' is', search_on, '.')
-st.subheader(fruit_chosen + 'Nutrition Information')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        search_on=pd_df.loc [pd_df ['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+         
+        st.write('The search value for', fruit_chosen,' is', search_on, '.')
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
 
 
 
